5673bc7b5268450932517ca1210089523e00c1eb
ramonaharrison <ramona.e.harrison@gmail.com>
2015-03-07 16:03:49 -0500

    added greeting responder

---
+++ src/nyc/c4q/ramonaharrison/GreetingResponder.java
@@ -0,0 +1,29 @@
+package nyc.c4q.ramonaharrison;
+
+/**
+ * Access Code 2.1
+ * Ramona Harrison
+ * GreetingResponder.java
+ * This class responds to greetings
+ */
+
+import java.util.Scanner;
+
+public class GreetingResponder {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String greeting1;
+        String answer = "";
+
+        System.out.print("Say hello in your favorite language:");
+        greeting1 = input.next();
+        System.out.print(greeting1 + "? Is that Spanish?");
+        answer = input.next();
+        if (answer.equals("yes")) {
+            System.out.print("How lovely!");
+        } else {
+            System.out.print("Oops!");
+        }
+
+    }
+}
